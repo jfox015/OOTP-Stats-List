93fcf2341ca35dea68b969d654d83dc030f74a64
Jeff Fox <jfox015@gmail.com>
2012-06-13 23:37:06 -0400

    Updating tests to use new Stats Class

--- controllers/statslist.php
+++ controllers/statslist.php
@@ -10,14 +10,16 @@ class statslist extends Front_Controller {
 		
 		$settings = $this->settings_lib->find_all();
 		
-		$this->load->model('ootp_web_toolkit/leagues_model');
-		$this->load->model('ootp_web_toolkit/teams_model');
+		$this->load->model('open_sports_toolkit/leagues_model');
+		$this->load->model('open_sports_toolkit/teams_model');
 		
 		$years = $this->leagues_model->get_all_seasons($settings['ootp.league_id']);
 		
 		if ($this->input->post('submit')) 
 		{
-			$team_id = ($this->input->post('team_id') ? $this->input->post('team_id') : false);
+            $this->load->library('open_sports_toolkit/stats');
+            Stats::init('baseball','ootp13');
+            $team_id = ($this->input->post('team_id') ? $this->input->post('team_id') : false);
 			$year = ($this->input->post('year')? $this->input->post('year') : false);
 			
 			if ($team_id !== false)
@@ -40,11 +42,11 @@ class statslist extends Front_Controller {
 						$league_year = date('Y',$currDate);
 					}
 				}
-				$this->load->model('ootp_web_toolkit/players_model');
+				$this->load->model('open_sports_toolkit/players_model');
                 $records = array (
-					'Batting'=>$this->players_model->get_current_stats(2,$settings['ootp.league_id'], $league_year, $team_id),
-					'Pitching'=>$this->players_model->get_current_stats(1,$settings['ootp.league_id'], $league_year, $team_id)
-				);
+                    'Batting'=>$this->players_model->get_current_player_stats(231,TYPE_OFFENSE,CLASS_STANDARD,array('season'=>$league_year,'limit'=>1)),
+                    'Pitching'=>$this->players_model->get_current_player_stats(311,TYPE_SPECIALTY,CLASS_STANDARD,array('season'=>$league_year,'limit'=>1))
+                );
 				Template::set('records',$records);
 				Template::set('league_year',$league_year);
 				Template::set('team_id',$team_id);
@@ -53,7 +55,7 @@ class statslist extends Front_Controller {
 		}
 		$this->load->helper('form');
 		$this->load->helper('url');
-		$this->load->helper('ootp_web_toolkit/general');
+		$this->load->helper('open_sports_toolkit/general');
         Template::set('teams',$this->teams_model->get_teams_array(($settings['statslist.limit_to_primary'] == 1) ? $settings['ootp.league_id'] : false));
 		Template::set('years',$years);
 		Template::set('settings',$settings);
@@ -64,11 +66,19 @@ class statslist extends Front_Controller {
     public function stats_test()
     {
         $this->load->library('open_sports_toolkit/stats');
-        Stats::init('baseball','ootp');
+        Stats::init('baseball','ootp13');
         Template::set('stats_list', Stats::get_stats_list());
-        Template::set('player_query', Stats::get_player_stats(231,TYPE_OFFENSE,CLASS_STANDARD,STATS_CAREER,array('limit',10)));
-
+        $this->load->model('open_sports_toolkit/players_model');
+        
+		Template::set('batting_query', $this->players_model->get_current_player_stats(231,TYPE_OFFENSE,CLASS_STANDARD,array('limit',10)));
+		Template::set('pitching_query', $this->players_model->get_current_player_stats(311,TYPE_SPECIALTY,CLASS_STANDARD,array('limit',10)));
+        $records = array (
+            'Batting'=>$this->players_model->get_current_player_stats(231,TYPE_OFFENSE,CLASS_STANDARD,array('limit',1)),
+            'Pitching'=>$this->players_model->get_current_player_stats(311,TYPE_SPECIALTY,CLASS_STANDARD,array('limit',1))
+        );
+        Template::set('records',$records);
         Template::set_view('statslist/test');
+        Template::set_view('statslist/index');
         Template::render();
     }
 }
--- views/test.php
+++ views/test.php
@@ -4,6 +4,7 @@
 	<div class="row-fluid rowbg content">
     <div><?php echo("AB Stat descript = ".lang("AB")."<br/ >"); ?></div>
     <div><?php echo("AVG formula = ".implode(",",$stats_list['offense']['HR'])."<br/ >"); ?></div>
-    <div><?php echo("PLayer SQL  = <br />".$player_query); ?></div>
+    <div><?php echo("Batting SQL  = <br />".$batting_query."<br/ >"); ?></div>
+    <div><?php echo("Pitching SQL  = <br />".$pitching_query."<br/ >"); ?></div>
 	</div>
 </div>
